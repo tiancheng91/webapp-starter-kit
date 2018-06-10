aed7d6f7d86ebcdef06cbfb6fc273917081344be
tiancheng91 <tiancheng91@gmail.com>
2018-06-10 12:34:26 +0800

    更新 支持静态文件node_modules引入

--- gulpfile.js
+++ gulpfile.js
@@ -131,7 +131,8 @@ gulp.task('serve', () => {
       server: {
         baseDir: ['.tmp', 'app'],
         routes: {
-          '/bower_components': 'bower_components'
+          '/bower_components': 'bower_components',
+          '/node_modules': 'node_modules'
         }
       }
     });
